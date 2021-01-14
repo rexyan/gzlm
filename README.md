d14839cb04cd2ee973021707fec9ffd02950b48f
runsha.yan <runsha.yan@cyclone-robotics.com>
2021-01-14 18:38:40 +0800

    up

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
+### 本地环境开发
+1. 下载项目
+```
+git clone git@github.com:rexyan/gzlm.git
+```
+
+2. 安装依赖
+```
+pip install -r requirements. txt 
+```
+
+3. 同步表结构
+```
+python manage.py makemigrations
+python manage.py migrate
+```
+
+3. 启动项目
+```
+python manage.py runserver 8000
+```
+
+### 导入数据
+
+
+### 切换数据库
+BMS/setting.py 
+```
+使用 sqlite3
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+```
+
+使用 MySQL
+```
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bms',  # 数据库名
+        'HOST': '127.0.0.1',   # 数据库地址
+        'PASSWORD': '123456',  # 数据库密码
+        'PORT': 3306,   # 数据库端口
+        'USER': 'root', # 数据库账号
+    }
+}
+```
+
+
 
--- index/models.py
+++ index/models.py
@@ -3,8 +3,23 @@ from django.db import models
 
 # Create your models here.
 class GZLM(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=32)
+    id = models.AutoField(primary_key=True, auto_created=True)
+    company_name = models.CharField(max_length=100)
+    date = models.CharField(max_length=100)
+    number_plate = models.CharField(max_length=100)
+    driver = models.CharField(max_length=100)
+    id_card1 = models.CharField(max_length=100)
+    contact_detail1 = models.CharField(max_length=100)
+    driver2 = models.CharField(max_length=100)
+    id_card2 = models.CharField(max_length=100)
+    contact_detail2 = models.CharField(max_length=100)
+    issued_area = models.CharField(max_length=100)
+    arrival_area = models.CharField(max_length=100)
+    arrival_time = models.CharField(max_length=100)
+    leave_time = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    car_attribution = models.CharField(max_length=100)
+    remark = models.CharField(max_length=100)
 
 
 
